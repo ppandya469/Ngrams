6c7d74f3c52c908c8d2656df6d501bc1f476f624
Jordan <jordansjsalazar@gmail.com>
2023-03-20 17:45:31 -0700

    setup methods

--- proj2b/ngordnet/hyponyms/HyponymMap.java
+++ proj2b/ngordnet/hyponyms/HyponymMap.java
@@ -1,5 +1,7 @@
 package ngordnet.hyponyms;
 
+import edu.princeton.cs.algs4.In;
+
 import java.util.TreeMap;
 
 public class HyponymMap {
@@ -8,7 +10,19 @@ public class HyponymMap {
     private TreeMap<Integer, String> wordIDs;
 
     public HyponymMap(String synFile, String hypFile) {
-        
+        In syns = new In(synFile);
+        In hyps = new In(hypFile);
+        while (hyps.hasNextLine()) {
+            if (hyps.isEmpty()) {
+                break;
+            }
+            String[] arr = hyps.readString().split(",");
+            int id = Integer.valueOf(arr[0]);
+            String words = arr[1];
+            wordIDs.put(id, words);
+        }
+        synsets = new DirectedGraph(wordIDs.keySet());
+        // read the synsets into the graph
     }
 
 }
