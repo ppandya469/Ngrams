e3ef21a46d45ce20a01074715f332826584994d7
Pari <ppandya469@berkeley.edu>
2023-03-20 17:09:40 -0700

    new file

---
+++ proj2b/ngordnet/main/HyponymsHandler.java
@@ -0,0 +1,23 @@
+package ngordnet.main;
+
+import ngordnet.browser.NgordnetQuery;
+import ngordnet.browser.NgordnetQueryHandler;
+import ngordnet.ngrams.NGramMap;
+import spark.QueryParamsMap;
+import spark.Request;
+import spark.Response;
+
+public class HyponymsHandler extends NgordnetQueryHandler {
+
+        private NGramMap ngm;
+
+        public HyponymsHandler(NGramMap n) {
+            ngm = n;
+        }
+
+        @Override
+        public String handle(NgordnetQuery q) {
+            return "Hello!";
+        }
+
+}
