caceb8041513eb5efc571861fc30389a1414ee0d
Jordan <jordansjsalazar@gmail.com>
2023-03-23 21:21:55 -0700

    Fixed graph

--- proj2b/ngordnet/hyponyms/HyponymMap.java
+++ proj2b/ngordnet/hyponyms/HyponymMap.java
@@ -34,4 +34,5 @@ public class HyponymMap {
 
         return;
     }*/
+
 }
--- proj2b/ngordnet/main/HyponymsHandler.java
+++ proj2b/ngordnet/main/HyponymsHandler.java
@@ -8,6 +8,7 @@ import spark.QueryParamsMap;
 import spark.Request;
 import spark.Response;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HyponymsHandler extends NgordnetQueryHandler {
@@ -21,7 +22,13 @@ public class HyponymsHandler extends NgordnetQueryHandler {
         @Override
         public String handle(NgordnetQuery q) {
             List<String> words = q.words();
-            String word = words.get(0);
-            return hm.hyponyms();
+            String w = words.get(0);
+            ArrayList<String> h = hm.hyponyms(w);
+            String tr = "";
+            for (int i = 0; i < h.size() - 1; i++) {
+                tr += (h.get(i) + ", ");
+            }
+            tr += (h.get(h.size() - 1));
+            return tr;
         }
 }
